0fc4e06ce4abbee759a9e0fea7e6e416b2eae2be
Royweru <royezi17@gmail.com>
2023-11-07 08:08:53 +0300

    new file:   app/(root)/page.tsx 	modified:   app/globals.css 	modified:   app/layout.tsx 	deleted:    app/page.tsx 	new file:   components/navbar.tsx 	new file:   components/search-bar.tsx 	new file:   components/ui/button.tsx 	modified:   package-lock.json 	modified:   package.json 	new file:   public/images/bg1.jpeg 	new file:   public/images/logo.jpg

---
+++ components/search-bar.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className=' rounded-lg px-4 py-10 flex justify-center items-center w-full m-5 bg-gray-500'>
+        <div className=' grid grid-cols-2 h-full w-full gap-4'>
+             <div>
+                input
+             </div>
+             <div>
+                input
+             </div>
+             <div>
+                input
+             </div>
+             <div>
+                input
+             </div>
+             <div>
+                input
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default SearchBar
