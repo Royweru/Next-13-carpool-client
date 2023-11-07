0bc36bde610ede21661f7f50e69626dec66529f4
Royweru <royezi17@gmail.com>
2023-11-07 09:53:00 +0300

    modified:   app/(root)/page.tsx 	new file:   components/desc.tsx 	modified:   components/search-bar.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import SearchBar from '@/components/search-bar'
+import Desc from '@/components/desc'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <div className=" grid grid-cols-1 md:grid-cols-2 p-5 gap-8 mb-5 w-full mt-5 h-2/6">
           <SearchBar />
+          <Desc />
       </div>
     </main>
   )
--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -2,23 +2,33 @@ import React from 'react'
 
 const SearchBar = () => {
   return (
-    <div className=' rounded-lg px-4 py-10 flex justify-center items-center w-full m-5 bg-gray-500'>
+    <div className=' rounded-lg px-4 py-12 flex justify-center items-center w-full m-5 bg-slate-300'>
         <div className=' grid grid-cols-2 h-full w-full gap-4'>
-             <div>
-                input
-             </div>
-             <div>
-                input
-             </div>
-             <div>
-                input
-             </div>
-             <div>
-                input
-             </div>
-             <div>
-                input
-             </div>
+          <input
+                type="text"
+                placeholder="Model"
+                className="w-full  p-3 py-2 text-black bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+                <input
+                type="text"
+                placeholder="Make"
+                className="w-full pl-12 pr-3 py-2 text-black bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+               <input
+                type="text"
+                placeholder="Body Type"
+                className="w-full pl-12 pr-3 py-2 text-black bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+               <input
+                type="text"
+                placeholder="Color"
+                className="w-full pl-12 pr-3 py-2 text-black bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
+               <input
+                type="text"
+                placeholder="Year of Manufacture"
+                className="w-full pl-12 pr-3 py-2 text-black bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+            />
         </div>
     </div>
   )
