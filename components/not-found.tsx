2927ff78808cf92e648cf38598c64e8793a29ae1
Royweru <royezi17@gmail.com>
2023-11-12 13:10:56 +0300

    modified:   components/not-found.tsx

--- components/not-found.tsx
+++ components/not-found.tsx
@@ -6,12 +6,12 @@ import { useRouter } from 'next/navigation'
 const NotFound = () => {
     const router = useRouter()
   return (
-    <div className='flex justify-center items-center w-full'>
-      <div className=' p-7 border border-transparent rounded flex-col justify-center items-center bg-black'>
+    <div className='flex justify-center items-center w-full mb-5'>
+      <div className=' p-7 border border-transparent rounded flex-col space-y-3  justify-center items-center bg-black'>
            <h1 className=' text-3xl text-white font-bold font-serif'>
              OOPS! car you are looking for not found
            </h1>
-           <div>
+           <div className=' w-full flex justify-center items-center'>
               <Button variant="destructive" size="lg" onClick={()=>router.push('/cars')}>
                 Remove filters
               </Button>
